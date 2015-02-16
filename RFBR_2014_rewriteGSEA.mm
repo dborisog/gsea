3ccc26a256ab374a8193c8001b8e374ba02c9cea
dborisog <dmitry.borisoglebsky@gmail.com>
2015-02-16 20:02:55 +0200

    add mindmap

---
+++ RFBR_2014_rewriteGSEA.mm
@@ -0,0 +1,201 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1422851904902" ID="ID_1176669161" MODIFIED="1422855799291">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      rewrite
+    </p>
+    <p>
+      GSEA
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="15"/>
+<node CREATED="1422855221552" ID="ID_877559119" MODIFIED="1422859057508" POSITION="right" TEXT="model">
+<node CREATED="1422855228148" ID="ID_516124433" MODIFIED="1422855794533">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="RFBR_2014_model.png" />
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="5"/>
+</node>
+</node>
+<node CREATED="1422851944158" ID="ID_211502734" MODIFIED="1422862421493" POSITION="left" TEXT="loadData()">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1422855697221" FOLDED="true" ID="ID_857778553" MODIFIED="1423349550157" POSITION="left" TEXT="extractFactors()">
+<icon BUILTIN="button_ok"/>
+<icon BUILTIN="help"/>
+<node CREATED="1422855868371" ID="ID_778240621" MODIFIED="1422859570972" TEXT="Actually, not sure if this function is required &#xa;though would definitely help to define contrasts and check if setup is OK ">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1422853202539" FOLDED="true" ID="ID_12823600" MODIFIED="1422988641914" POSITION="left" TEXT="plotPCA()">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1422862439215" ID="ID_240434030" MODIFIED="1422862468726" TEXT="no output defined. this function just generates a new plot">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1422853210462" FOLDED="true" ID="ID_1909518369" MODIFIED="1422988643816" POSITION="left" TEXT="plotHist()">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1422862439215" ID="ID_1560319480" MODIFIED="1422862468726" TEXT="no output defined. this function just generates a new plot">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1422853214734" FOLDED="true" ID="ID_51436733" MODIFIED="1423349544379" POSITION="left" TEXT="plotBox()">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1422862439215" ID="ID_871423954" MODIFIED="1422862468726" TEXT="no output defined. this function just generates a new plot">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1422873764549" ID="ID_628010782" MODIFIED="1422873891770" TEXT="black colour only -- difficult to identify the bad vector">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1422851949848" ID="ID_48893852" MODIFIED="1423349541181" POSITION="left" TEXT="exprDiff()">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1422897230266" ID="ID_1234645991" MODIFIED="1422897275538" TEXT="still have NA values, though might remove them in the future">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1422853221352" FOLDED="true" ID="ID_1408643653" MODIFIED="1423726627619" POSITION="left" TEXT="plotVenn()">
+<icon BUILTIN="button_cancel"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1423066688435" ID="ID_1595629199" MODIFIED="1423726580901" TEXT="numbers do not add up">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1423726536000" ID="ID_1775914233" MODIFIED="1423726580901" TEXT="combine up &amp; down graphs into one">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1422852665875" FOLDED="true" ID="ID_1795803023" MODIFIED="1423049119369" POSITION="left" TEXT="loadSets()">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1423042828263" ID="ID_1080205652" MODIFIED="1423042952359" TEXT="rfbr_orthologs">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1423042853039" ID="ID_1968304057" MODIFIED="1423044866321" TEXT="kegg">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1423042862814" ID="ID_1793641934" MODIFIED="1423047777149">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      aging_diseases
+    </p>
+    <p>
+      aging_pathways
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1423042889042" ID="ID_647602542" MODIFIED="1423049107662" TEXT="GO:BP, GO:MF, GO:CC">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1423042917118" ID="ID_1532158857" MODIFIED="1423049112274" TEXT="filtering?">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1422851955447" ID="ID_802957777" MODIFIED="1423349539720" POSITION="left" TEXT="enrichSets()">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1422853227965" FOLDED="true" ID="ID_556423007" MODIFIED="1423726629449" POSITION="left" TEXT="networkPlot()">
+<icon BUILTIN="button_cancel"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1423396788005" ID="ID_1058341602" MODIFIED="1423726407066" TEXT="up and down regulated">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1423726184583" ID="ID_805341353" MODIFIED="1423726422942" TEXT="node size is driven by No of DE genes in gset">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1423726204774" ID="ID_1370785481" MODIFIED="1423726422942" TEXT="edge width is driven by ">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1423726411423" ID="ID_1428285727" MODIFIED="1423726422941" TEXT="add DE genes to the graph">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1423726084944" ID="ID_1459962194" MODIFIED="1423726485878" TEXT="table of up &amp; down DE genes">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1422853234539" ID="ID_1174396967" MODIFIED="1423726615116" POSITION="left" TEXT="heatmapPlot()">
+<icon BUILTIN="idea"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1423368808154" ID="ID_526777496" MODIFIED="1423845038379" TEXT="up and down regulated">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1423755001077" ID="ID_1095746954" MODIFIED="1423755005870" TEXT="significant gene sets, any-regulated">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1423755001077" ID="ID_1197326561" MODIFIED="1423755005870" TEXT="significant gene sets, up-regulated">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1423755001077" ID="ID_152351488" MODIFIED="1423755005870" TEXT="significant gene sets, down-regulated">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1423755001077" ID="ID_282888881" MODIFIED="1423768237286" TEXT="DE-related gene sets, any-regulated">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1423755001077" ID="ID_1538046660" MODIFIED="1423770328952" TEXT="DE-related gene sets, up-regulated">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1423755001087" ID="ID_354954108" MODIFIED="1423770328953" TEXT="DE-related gene sets, down-regulated">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1423768228865" ID="ID_151675028" MODIFIED="1423771448479">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <span style="text-indent: 0px; display: inline !important; font-weight: normal; background-color: rgb(255, 255, 255); color: rgb(20, 24, 35); font-variant: normal; text-align: left; word-spacing: 0px; white-space: pre-wrap; font-style: normal; letter-spacing: normal; font-size: 13px; float: none; line-height: 16.3090915679932px; font-family: Helvetica, Arial, lucida grande, tahoma, verdana, arial, sans-serif; text-transform: none"><font color="rgb(20, 24, 35)" size="13px" face="Helvetica, Arial, lucida grande, tahoma, verdana, arial, sans-serif">library(&quot;RColorBrewer&quot;) colorRampPalette(brewer.pal(9, &quot;GnBu&quot;))(100)</font></span>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1423726149978" ID="ID_1702454127" MODIFIED="1423845028763" TEXT="padj &amp; pval heatmaps">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1423726084944" ID="ID_1683521141" MODIFIED="1423845041531" TEXT="table of up &amp; down DE genes">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node CREATED="1422897369369" ID="ID_1812239046" MODIFIED="1423378979560" POSITION="left" TEXT="new functions">
+<icon BUILTIN="help"/>
+<node CREATED="1422855500302" ID="ID_123340316" MODIFIED="1422855520074" TEXT="define working directory">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1422897384713" ID="ID_1245783369" MODIFIED="1422897391269" TEXT="clean saved files">
+<node CREATED="1422897395032" ID="ID_528362525" MODIFIED="1422897417861" TEXT="though that might be an option at the functions like diffExpr()"/>
+</node>
+</node>
+<node CREATED="1422853186407" ID="ID_543767184" MODIFIED="1423378985911" POSITION="left" TEXT="genesets">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1422988224945" ID="ID_887154665" MODIFIED="1423045029689" TEXT="prepareGSets.R"/>
+</node>
+<node CREATED="1422862403742" ID="ID_574879412" MODIFIED="1422862408091" POSITION="right" TEXT="generic issues">
+<node CREATED="1422855601002" ID="ID_74781105" MODIFIED="1422855721692" TEXT="no quality checks">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1422897485848" ID="ID_1370886594" MODIFIED="1422897491629" TEXT="remove old functions"/>
+</node>
+<node CREATED="1423726647116" ID="ID_1090040585" MODIFIED="1423726651811" POSITION="right" TEXT="simply tasks">
+<node CREATED="1423726654515" ID="ID_334883947" MODIFIED="1423726724763" TEXT="combine old and new research in a heatmap"/>
+<node CREATED="1423396907405" ID="ID_1942548147" MODIFIED="1423726386278" TEXT="report">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1423726738238" ID="ID_29683244" MODIFIED="1423726771762" TEXT="single venn diagram, multiple replicas are not that useful"/>
+</node>
+</node>
+</node>
+</map>
